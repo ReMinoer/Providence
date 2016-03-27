20484815f77c50db327c2ebae8081a70438d46ee
Rémi Morillon <rmorillon@freneticexperience.com>
2016-03-27 01:31:17 +0100

    Initial commit with default window behaviour

---
+++ Providence/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Providence
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Providence/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Gma.System.MouseKeyHook;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+
+namespace Providence
+{
+    public partial class MainWindow
+    {
+        private readonly IKeyboardEvents _keyboardEvents;
+        private bool _winModifier;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _keyboardEvents = Hook.GlobalEvents();
+            _keyboardEvents.KeyDown += KeyboardEventsOnKeyDown;
+            _keyboardEvents.KeyUp += KeyboardEventsOnKeyUp;
+
+            TextBox.Focus();
+            TextBox.SelectAll();
+
+            Left = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Left + (System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width - Width) / 2;
+        }
+
+        private void ShowBar()
+        {
+            TextBox.Focus();
+            TextBox.SelectAll();
+
+            Show();
+        }
+
+        private void TextBox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Hide();
+            }
+            else if (e.Key == Key.Escape)
+                Hide();
+        }
+
+        private void TextBox_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBox.SelectAll();
+            e.Handled = true;
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            base.OnDeactivated(e);
+            Hide();
+        }
+
+        private void KeyboardEventsOnKeyDown(object sender, System.Windows.Forms.KeyEventArgs keyEventArgs)
+        {
+            if (IsVisible)
+                return;
+
+            if (keyEventArgs.KeyCode == System.Windows.Forms.Keys.LWin)
+                _winModifier = true;
+
+            if (_winModifier && keyEventArgs.KeyCode == System.Windows.Forms.Keys.Space)
+                ShowBar();
+        }
+
+        private void KeyboardEventsOnKeyUp(object sender, System.Windows.Forms.KeyEventArgs keyEventArgs)
+        {
+            if (keyEventArgs.KeyCode == System.Windows.Forms.Keys.LWin)
+                _winModifier = false;
+        }
+
+        private void ExitMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
